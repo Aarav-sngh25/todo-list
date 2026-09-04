9aca2118ccceb5b6e220caff1ab8ee04a8a09a0c
Aarav Singh <aaravsingh252009@gmail.com>
2026-09-04 12:12:49 +0530

    Add todo edit functionality

--- script.js
+++ script.js
@@ -1,77 +1,102 @@
-
-const todoList = JSON.parse(localStorage.getItem('todo')) || [];
+const todoList = JSON.parse(localStorage.getItem("todo")) || [];
+let editingIndex;
 
 renderTodoList();
 
 function renderTodoList() {
-  let todoListHTML = '';
+  let todoListHTML = "";
 
   todoList.forEach((todo, i) => {
-    const {name, dueDate} = todo;
+    const { name, dueDate } = todo;
     const html = `
-      <div>${name}</div>
+      <div>${
+        editingIndex === i
+          ? `<input class="edit-input js-edit-input" 
+        value=${name}>`
+          : name
+      }</div>
       <div>${dueDate}</div>
+      ${
+        editingIndex === i
+          ? `<button class="confirm-button js-confirm-button">Confirm</button>`
+          : `<button class="edit-button js-edit-button">Edit</button>`
+      }
       <button class="delete-button js-delete-button">Delete</button>
       `;
     todoListHTML += html;
   });
 
-  document.querySelector('.js-todo-list').innerHTML = todoListHTML;
+  document.querySelector(".js-todo-list").innerHTML = todoListHTML;
 
-  localStorage.setItem('todo', JSON.stringify(todoList));
+  localStorage.setItem("todo", JSON.stringify(todoList));
 
-  document.querySelectorAll('.js-delete-button')
-    .forEach((deleteButton, i) => {
-      deleteButton.addEventListener('click', () => {
-        todoList.splice(i, 1);
-        renderTodoList();
-      })
+  document.querySelectorAll(".js-delete-button").forEach((deleteButton, i) => {
+    deleteButton.addEventListener("click", () => {
+      todoList.splice(i, 1);
+      renderTodoList();
     });
+  });
+
+  document.querySelectorAll(".js-edit-button").forEach((editButton, i) => {
+    editButton.addEventListener("click", () => {
+      editingIndex = i;
+      renderTodoList();
+    });
+  });
+
+  if (editingIndex || editingIndex === 0) {
+    document
+      .querySelector(".js-confirm-button")
+      .addEventListener("click", () => {
+        todoList[editingIndex].name =
+          document.querySelector(".js-edit-input").value;
+        editingIndex = undefined;
+        renderTodoList();
+      });
+  }
 }
 
 function addTodo() {
-  const inputElem = document.querySelector('.js-name-input');
+  const inputElem = document.querySelector(".js-name-input");
   const name = inputElem.value;
 
-  const dateInputElem = document.querySelector('.js-due-date-input');
+  const dateInputElem = document.querySelector(".js-due-date-input");
   const dueDate = dateInputElem.value;
 
-  if(!name) {
+  if (!name) {
     return;
   }
-    
+
   todoList.push({
     name: name,
-    dueDate: dueDate
-    });
+    dueDate: dueDate,
+  });
 
-  inputElem.value = '';
-  dateInputElem.value = '';
-  
-	renderTodoList();
+  inputElem.value = "";
+  dateInputElem.value = "";
+
+  renderTodoList();
 }
 
 function handleEnterButton(event) {
-  if(event.key === 'Enter') {
-    addTodo()
+  if (event.key === "Enter") {
+    addTodo();
   }
 }
 
-document.querySelector('.js-add-button')
-  .addEventListener('click', () => {
-    addTodo();
-  })
+document.querySelector(".js-add-button").addEventListener("click", () => {
+  addTodo();
+});
 
-document.querySelectorAll('.js-input')
-.forEach((input) => {
-  input.addEventListener('keydown', (event) => {
-  handleEnterButton(event);
+document.querySelectorAll(".js-input").forEach((input) => {
+  input.addEventListener("keydown", (event) => {
+    handleEnterButton(event);
   });
 });
 
-document.body.addEventListener('keydown', (event) => {
-  if(event.key === 'Delete') {
-  todoList.splice(todoList.length - 1, 1);
-  renderTodoList();
-  };
+document.body.addEventListener("keydown", (event) => {
+  if (event.key === "Delete") {
+    todoList.splice(todoList.length - 1, 1);
+    renderTodoList();
+  }
 });
